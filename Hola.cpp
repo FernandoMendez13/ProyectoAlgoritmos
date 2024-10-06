f5206a69c3d2f51ec56dbf48ed8592fb8c07c362
FernandoMendez13 <fernandomendeeez13@gmail.com>
2024-10-05 20:57:43 -0600

    Menu

--- Hola.cpp
+++ Hola.cpp
@@ -1,11 +1,34 @@
-//Prueba
 #include <iostream>
 
 using namespace std;
 
-int main () {
+void menu () {
+    cout << "Menu" << endl << "1: Suma" << endl;
+}
 
-    cout << "Hola mundo";
+int suma (int a, int b){
+    int resultado = a + b;
+    return resultado;
+}
+int main () {
+    int opcion;
+
+    do {
+        menu ();
+        cout << "Ingrese una opcion: " << endl;
+        cin >> opcion;
+        if (opcion == 1) {
+            int a;
+            int b;
+            int resultado;
+            cout << "Ingrese un numero:" << endl;
+            cin >> a;
+            cout << "Ingrese un numero:" << endl;
+            cin >> b;
+            resultado = suma(a,b);
+            cout << "El resultado de la suma es: " << resultado;
+        }
+    } while ( opcion != 2);
 
     return 0;
 }
